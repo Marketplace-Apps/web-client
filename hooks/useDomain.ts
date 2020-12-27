1b8973293febe8fbdcc0ea10fd19777865c9abfe
duongvanba <duongvanba@duongvanba.pc>
2020-12-27 20:19:51 +0700

    Update

--- hooks/useDomain.ts
+++ hooks/useDomain.ts
@@ -2,7 +2,7 @@ import { useDocumentData } from "react-livequery-hooks"
 import { Domain } from "../types"
 
 export const useDomain = (ref?: string) => {
-    const { item, error, loading } = useDocumentData<Domain>('domains/~~', {
+    const { item, error, loading } = useDocumentData<Domain>('domains/current', {
         cache: { use: true, update: true },
         reatime: false
     })
