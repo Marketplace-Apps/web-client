35835c85e511f25514c260edf7c64a4053c7eb59
Ba <duongvanba.com@gmail.com>
2021-01-31 19:09:30 +0700

    asdvgsdf

--- components/services/ServiceItem.tsx
+++ components/services/ServiceItem.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation"
 import { useRouter } from "next/router"
 import { Badge } from "react-bootstrap"
 import { useHover } from "../../hooks/useHover"
@@ -13,16 +14,17 @@ export const ServiceItem = ({ service, onClick }: ServiceItem) => {
 
     const router = useRouter()
     const { hovering, listeners } = useHover()
+    const { t } = useTranslation('common')
 
     return (
         <div
-            onClick={onClick as any}
+            onClick={!service.maintain ? onClick as any : () => { }}
             className="d-flex flex-column justify-content-start align-items-center align-content-center"
             style={{
                 height: 120,
-                cursor: 'pointer',
+                cursor: service.maintain ? 'not-allowed' : 'pointer',
                 padding: '15px 0 5px 0',
-                boxShadow: hovering ? 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px' : ' rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+                boxShadow: !service.maintain && hovering ? 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px' : ' rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
                 borderRadius: 10
             }}
             {...listeners}
@@ -32,15 +34,17 @@ export const ServiceItem = ({ service, onClick }: ServiceItem) => {
                     src={service.icon}
                     width={40}
                     height={40}
+                    style={service.maintain ? { filter: 'grayscale(1.0)' } : {}}
                 />
                 <Badge
                     pill
-                    variant="danger"
+                    variant={service.maintain ? 'dark' : "danger"}
                     style={{ position: 'absolute', top: 15, marginLeft: 40 }}
                 >{service.promote_price} đ </Badge>
             </div>
             <div className="text-center" style={{ fontSize: 17, color: '#0a6cab' }}>
                 {service.name[router.locale]}
+                {service.maintain && <Badge style={{ fontSize: 10, marginLeft: 5 }} variant="dark" >{t('maintain')}</Badge>}
             </div>
         </div>
     )
