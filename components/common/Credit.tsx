4b55abe234b555e326df37b2c3407a58598e1128
Ba <duongvanba.com@gmail.com>
2021-02-23 15:10:58 +0700

    Update

--- components/common/Credit.tsx
+++ components/common/Credit.tsx
@@ -3,9 +3,14 @@ import { useRouter } from "next/router"
 export type Credit = {
     value?: number
 }
-export const Credit = ({value = 0}: Credit) => {
+export const Credit = ({ value }: Credit) => {
+
+ 
 
     const { locale } = useRouter()
+
+    if (value == undefined || value == null) return null
+    
     if (locale == 'en') return <span>{Number((value * 0.00005).toFixed(6))} $</span>
     return <span>{value.toLocaleString()} đ</span>
 }
